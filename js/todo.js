da73498a50f45839e1b1cb53f3ce3159a491e9fe
[igor-strbac] <[igor.shtrbac@gmail.com]>
2020-08-02 05:09:59 +0200

    Initial commit

---
+++ js/todo.js
@@ -0,0 +1,160 @@
+var ToDo = function (rootElementAll, rootElementActive, rootElementCompleted) {
+
+    this.rootElementAll = rootElementAll;
+    this.rootElementActive = rootElementActive;
+    this.rootElementCompleted = rootElementCompleted;
+
+    let ToDoItem = function (content, date) {
+        this.id = Math.random().toString(36).substring(7);
+        this.content = content;
+        this.date = date;
+        this.completed = false;
+    };
+
+    let ToDoItemViewModel = function (toDoItem, views) {
+        this.data = toDoItem;
+        this.views = views;
+        this.toJSON = function () {
+            return this.data;
+        }
+    };
+
+    let toDoItems = [];
+
+    // TEMPLATE LITERALS - omogucava definisanje bez konkatenacije primenom ovih kosih navodnika
+    const TODO_ITEM_TEMPLATE = `
+		<div class="todo-item-date">
+			<span class="day">Nov</span>
+			<span class="month">2020</span>
+		</div>
+		<div class="todo-item-content">
+			<span class="data">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+		</div>
+		<span class="delete-btn" title="delete"></span>
+`;
+
+
+    function generateToDoItemView(toDoItem) {
+
+        //first, create root element
+        // kreiramo root element i dodajemo mu sadrzaj koji se definise u item template-u
+        let toDoItemRoot = document.createElement('div');
+        toDoItemRoot.classList.add("todo-item");
+        toDoItemRoot.setAttribute('data-id', toDoItem.id);
+        toDoItemRoot.innerHTML = TODO_ITEM_TEMPLATE;
+
+        toDoItemRoot.getElementsByClassName("day")[0].innerHTML = toDoItem.date.toLocaleString('default', {day: 'numeric'});
+        toDoItemRoot.getElementsByClassName("month")[0].innerHTML = toDoItem.date.toLocaleString('default', {month: 'short'});
+        var dataElem = toDoItemRoot.getElementsByClassName("data")[0].innerHTML = toDoItem.content;
+        toDoItemRoot.getElementsByClassName("delete-btn")[0].setAttribute('data-id', toDoItem.id);
+
+        //Proverava se da li  je stavka kompletirana za slucaj da dolazi iz neke baze, iako je za rucne unose defaul false;
+        toDoItemRoot.classList.add(toDoItem.completed ? "completed" : null);
+
+        //Obavlja se kloniranje dom elementa kako bi smo od svake stavke imali dva dom elementa.
+        //Prva ide u prvi tab gde su All-item
+        // Dok druga ide u zavisnosti od toga da li je kompletirana ili je u toku
+        let toDoItemRootCopy = toDoItemRoot.cloneNode(true);
+
+        //Prvi element se svakako dodaje u prvi tab
+        rootElementAll.append(toDoItemRoot);
+
+        //Ovde pravimo logiku koja smesta drugi dom element u aktivan ili kompletiran sadrzaj.
+        if (toDoItem.completed) {
+            rootElementCompleted.append(toDoItemRootCopy);
+        } else {
+            rootElementActive.append(toDoItemRootCopy);
+        }
+
+        let toDoItemViewModel = new ToDoItemViewModel(toDoItem, [toDoItemRoot, toDoItemRootCopy]);
+        toDoItems.push(toDoItemViewModel);
+
+        //register handlers for delete button
+        registerDeleteHandlers(toDoItemViewModel);
+
+        //register handlers for click on item
+        registerClickHandlers(toDoItemViewModel);
+    }
+
+    function registerDeleteHandlers(toDoViewModel) {
+        for (let i = 0; i < toDoViewModel.views.length; i++) {
+            toDoViewModel.views[i].getElementsByClassName("delete-btn")[0].onclick = function (e) {
+
+                e.stopPropagation();
+                var id = this.dataset.id;
+                var index = toDoItems.findIndex(item => item.data.id === id);
+
+                if (index > -1) {
+                    //remove from array and from DOM
+                    toDoItems.splice(index, 1);
+                    toDoViewModel.views[0].parentNode.removeChild(toDoViewModel.views[0]);
+                    toDoViewModel.views[1].parentNode.removeChild(toDoViewModel.views[1]);
+                }
+                saveToLocalStorage();
+            }
+        }
+    }
+
+    function registerClickHandlers(toDoItemViewModel) {
+        for (let i = 0; i < toDoItemViewModel.views.length; i++) {
+
+            toDoItemViewModel.views[i].onclick = function (e) {
+                var id = this.dataset.id;
+                var index = toDoItems.findIndex(item => item.data.id === id);
+
+                toDoItemViewModel.data.completed = !toDoItemViewModel.data.completed;
+
+                if (toDoItemViewModel.data.completed) {
+
+                    toDoItemViewModel.views[0].classList.add("completed");
+                    toDoItemViewModel.views[1].classList.add("completed");
+                    rootElementCompleted.appendChild(toDoItemViewModel.views[1]);
+
+                } else {
+                    toDoItemViewModel.views[0].classList.remove("completed");
+                    toDoItemViewModel.views[1].classList.remove("completed");
+
+                    rootElementActive.appendChild(toDoItemViewModel.views[1]);
+                }
+                saveToLocalStorage();
+            }
+        }
+    }
+
+    function saveToLocalStorage() {
+        localStorage.setItem('todo-data', JSON.stringify(toDoItems));
+    }
+
+    function loadFromLocalStorage() {
+
+        var json = localStorage.getItem('todo-data');
+
+        if (json === null)
+            return;
+
+        let toDoItems = JSON.parse(json, (key, value) => {
+            if (key === "date") {
+                value = new Date(value);
+            }
+            return value;
+        });
+
+        if (toDoItems.length === 0)
+            return;
+
+        for (let i = 0; i < toDoItems.length; i++) {
+            generateToDoItemView(toDoItems[i]);
+        }
+    }
+    loadFromLocalStorage();
+
+    //Primer closure-a
+    //Ukoliko ne stavimo u zagradi content na html-u u browseru je ispis [object HTMLInputElement] umesto teksta ispisa;
+    return {
+        add: function (content) {
+            let toDoItem = new ToDoItem(content, new Date());
+            generateToDoItemView(toDoItem);
+            saveToLocalStorage();
+        }
+    }
+};
